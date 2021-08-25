7b3bbca60f623e478b73ea431f01feca296d94c4
ybgandhi <gandhibyash@gmail.com>
2021-08-24 22:18:50 -0400

    Update HereosOfPy.py

--- HereosOfPy.py
+++ HereosOfPy.py
@@ -7,3 +7,8 @@ file_to_load = "Resources/purchase_data.csv"
 # set variable to read and establish a data frame
 purchase_date = pd.read_csv(file_to_load)
 purchase_date.head()
+
+# Player Count
+players_count = len(purchase_date["SN"].unique())
+print("Total number of players is:" ,players_count)
+ 
